403b25965c4323479adcd5676de1bee37b90b855
Greg Morgan <gregm9999@gmail.com>
2019-01-24 21:55:27 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require wrong_place_wrong_time
+
 //= require main
 //= require alert_maker
+//= require wrong_place_wrong_time
